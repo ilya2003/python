ca796f54a446dda1145d180e19d1f5f4ef8511f2
lyubomyr83 <lyubomyr83@mail.ru>
2019-12-07 18:58:41 +0300

    warnings

--- rodjer.py
+++ rodjer.py
@@ -41,19 +41,29 @@ def seconds_convert(time_in_seconds):
 
 
 def mode_select():
-    if os.path.exists(file_name):
-        mode = ''
+    mode = ''
+    if not os.path.exists(file_name):
+        
         print()
-        print('Работа над ошибками - 1')
-        print('Проверить знания в математике - 2')
+        print('Тренировка - 1')
+        print('выход - 0')
 
-        while mode not in {'1', '2'}:
+        while mode not in {'1', '0'}:
             print('Введите номер:')
             mode = input()
-            if mode not in {'1', '2'}:
-                print('Должно быть 1 или 2')
+            if mode not in {'1', '0'}:
+                print('Должно быть 1 или 0')
     else:
-        mode = '2'
+        print()
+        print('Работа над ошибками - 1')
+        print('Тренировка - 2')
+        print('выход - 0')
+
+        while mode not in {'1', '2','0'}:
+            print('Введите номер:')
+            mode = input()
+            if mode not in {'1', '2','0'}:
+                print('Должно быть 1, 2 или 0')
 
     return mode
 
@@ -64,7 +74,7 @@ def count():
         print('Давай проверим твои знания в математике.')
     sleep(1)
     
-        
+    my_warnings = ["Эх....", "А если подумать", "Проверь свой ответ прежде чем давать его", "Серьёзно?", "Напряги мозг"]   
     examples_quantity = ''  # Количество примеров
     count_to = ''  # До скольки будем считать
     correct_answers = 0  # правильные ответы
@@ -142,7 +152,8 @@ def count():
             print('Правильно, молодец')
             correct_answers += 1
         else:
-            print('Неправильно')
+       
+            print(my_warnings[randint(0, len(my_warnings)-1)])
             print('Правильный ответ: '+str(right_answer))
             fails += 1
             
@@ -150,9 +161,7 @@ def count():
             f = open(file_name, 'a')
 
             #  запишем ошибку в файл
-            if fails > 0:
-                f.write('\n')
-            f.write(f'{number1} {sign} {number2}')
+            f.write(f'{number1} {sign} {number2}\n')
 
     if fails == 0:
         print(f'Молодец, {name}! Ты правильно ответил на все примеры за {seconds_convert(answers_time)}')
@@ -166,11 +175,62 @@ def count():
 # Функция работы над ошибками
 def fix_errors():
 
+    correct_answers = 0  # правильные ответы
+    fails = 0  # ошибки
+    answers_time = 0  # затраченное время 
+
     with open(file_name, 'r') as f:
         line = f.readline()
-        separated_line = line.split()
-        print(separated_line)
-        number1, sign, number2 = separated_line
+
+        while line:
+            separated_line = line.split()
+            number1, sign, number2 = separated_line
+            
+            if  sign == '-':
+                right_answer = int(number1) - int(number2)
+
+            if  sign == '+':
+                right_answer = int(number1) + int(number2)
+
+            print('сколько будет ' + str(number1) + sign + str(number2))
+
+            answer = ''  # Ответ пользователя
+            while not answer.isdigit():
+
+                start = default_timer()  # начнем отсчёт
+                answer = input()
+                stop = default_timer()  # закончим отсчёт
+
+                answers_time += round(stop - start)
+
+
+                if not answer.isdigit():
+                    print('Должна быть цифра')
+                    print('сколько будет ' + str(number1) + sign + str(number2))
+
+            answer = int(answer)
+
+            if answer == right_answer:
+                print('Правильно, молодец')
+                correct_answers += 1
+            else:
+                print('Неправильно')
+                print('Правильный ответ: '+str(right_answer))
+                fails += 1
+                
+                # создадим временный файл для записи ошибок
+                with open(f'tmp_{file_name}', 'a') as f2:
+
+                    #  запишем ошибку в файл
+                    f2.write(f'{number1} {sign} {number2}\n')
+            
+
+            
+            # Получим следующую строку
+            line = f.readline()
+    os.remove(file_name)
+    if os.path.exists(f'tmp_{file_name}'):
+        os.rename(f'tmp_{file_name}', file_name)
 
 
 
@@ -183,15 +243,30 @@ sleep(1)
 
 file_name = f'{name}_errors.txt'
 
-start_type = mode_select()
+while True:
 
-# проверка знаний в математике
-if start_type == '1':
-    fix_errors()
-# работа над ошибками
-elif start_type == '2':
-    count()
-else:
-    pass
+    start_type = mode_select()
+
+    if not os.path.exists(file_name):
+        # проверка знаний в математике
+        if start_type == '1':
+            count()
+        # выход
+        elif start_type == '0':
+            break
+        else:
+            pass
+    else:
+        # работа над ошибками
+        if start_type == '1':
+            fix_errors()
+        # проверка знаний в математике
+        elif start_type == '2':
+            count()
+        # выход
+        elif start_type == '0':
+            break
+        else:
+            pass
     
     
