0a7a019f8169821f31d3e07df4da3f49a4b8a5cb
lyubomyr83 <lyubomyr83@mail.ru>
2020-03-28 19:01:23 +0300

    28032020

--- 21.py
+++ 21.py
@@ -1,4 +1,4 @@
-from random import shuffle
+from random import shuffle, choice
 from time import sleep
 from lib import check_input
 
@@ -15,7 +15,7 @@ def get_deck():
     return(deck)
 
 
-def get_card_points(card):
+def get_card_points(card, dealer=False):
     card_name = card.split()
 
     card_points = {}
@@ -25,91 +25,99 @@ def get_card_points(card):
         card_points[f'{card}'] = 10
     
     if card_name[0] == 'туз':
-        points = input('1 или 11?\n')
+        if dealer:
+            points = choice([1, 11])
+        else:
+            points = int(input('1 или 11?\n'))
     else:
         points = card_points[card_name[0]] 
 
     return points    
 
-deck = get_deck()
-for _ in range(20):
-    my_card = deck.pop()
-    points = get_card_points(my_card)
-    print(f'вам выпала карта {my_card}, очки {points}')
+
+balance = 100
+play_again = 'да'
+
+while play_again == 'да':
+    print(f'Баланс: {balance}$')
+    deposit = int(input('Ваша ставка?\n'))
+    if deposit > balance:
+        print('Депозит не может превышать баланс')
+        deposit = int(input('Ваша ставка?\n'))
+    elif balance == 0:
+        print('У вас закончились деньги')
+        break   
+    deck = get_deck()
+    your_points = 0
+    dealer_points = 0
+    max_point = 21
+    min_dealer_point = 17
+
+    move =1
+    more = 'да'
 
 
-
-
-# play_again = 'да'
-
-# while play_again == 'да':
-
-#     # deck = get_deck()
-#     your_points = 0
-#     dealer_points = 0
-#     max_point = 21
-#     min_dealer_point = 17
-
-#     move =1
-#     more = 'да'
-
-
-#     while more == 'да':
-#         dealer_card = cards.pop()
-#         dealer_points += dealer_card
+    while more == 'да':
         
-#         if move == 1:
-#             list_cards = []
-#             for  i in range(2):
-#                 your_card = cards.pop()
-#                 list_cards.append(your_card)
-#                 your_points += your_card
-#         else:
-#             your_card = cards.pop()
-#             your_points += your_card
+        if move == 1:
+            dealer_card = deck.pop()
+            dealer_points += get_card_points(dealer_card, True)
 
-#         print(f'''
-#         =============
-#         ОЧКИ:
-#         Крупье: {dealer_points}
-#         Вы: {your_points}
-#         ''')
+            list_cards = []
+            for  i in range(2):
+                your_card = deck.pop()
+                list_cards.append(your_card)
+                your_points += get_card_points(your_card)
+        else:
+            your_card = deck.pop()
+            your_points += get_card_points(your_card)
 
-#         if move == 1:
-#             print(f'Вам выпали карты номиналом {list_cards[0]} и {list_cards[1]}')
-#         else:
-#             print(f'Вам выпала карта номиналом {your_card}')
+        print(f'''
+        =============
+        ОЧКИ:
+        Крупье: {dealer_points}
+        Вы: {your_points}
+        =============
+        ''')
 
-        
-#         if your_points > max_point:
-#             print('Перебор')
-#             break
-#         elif your_points == max_point:
-#             more = 'нет'
-#         else:
-#             more = check_input(input('Ещё? да или нет\n'), ['да', 'нет'])
-#             move +=1
-
-#     else:
-#         while dealer_points <= min_dealer_point:
-#             dealer_card = cards.pop()
-#             dealer_points += dealer_card
-#             print(f'''
-#             =============
-#             ОЧКИ:
-#             Крупье: {dealer_points}
-#             Вы: {your_points}
-
-#             Крупье выпала карта номиналом {dealer_card}
-#             ''')
-#             sleep(3)
+        if move == 1:
+            print(f'Вам выпали карты {list_cards[0]} и {list_cards[1]}')
+        else:
+            print(f'Вам выпала карта {your_card}')
 
         
-#         if dealer_points > max_point:
-#                 print("Вы выиграли")
-#         elif your_points == dealer_points:
-#             print('Ничья')
-#         else:
-#             print('Крупье выиграл')
+        if your_points > max_point:
+            balance -= deposit
+            print('Перебор')
+            break
+        elif your_points == max_point:
+            more = 'нет'
+        else:
+            more = check_input(input('Ещё? да или нет\n'), ['да', 'нет'])
+            move +=1
 
-#     play_again = check_input(input('Сыграем ещё? да или нет\n'), ['да', 'нет']) 
+    else:
+        while dealer_points <= min_dealer_point:
+            dealer_card = deck.pop()
+            dealer_points += get_card_points(dealer_card, True)
+            print(f'Крупье выпала карта номиналом {dealer_card}')
+            sleep(3)
+
+        print(f'''
+            =============
+            ОЧКИ:
+            Крупье: {dealer_points}
+            Вы: {your_points}
+            =============
+            ''')
+        
+        if dealer_points > max_point:
+            balance += deposit
+            print("Вы выиграли")
+        elif your_points == dealer_points:
+            print('Ничья')
+        else:
+            balance += deposit
+            print('Вы выиграли')
+
+    play_again = check_input(input('Сыграем ещё? да или нет\n'), ['да', 'нет']) 
